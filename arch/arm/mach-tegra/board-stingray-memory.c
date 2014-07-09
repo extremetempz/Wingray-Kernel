e52a17b2b03ec7ddf50ae6cce9c7fee6cf44c1d2
ExtremeTempz <justinscollo1@gmail.com>
2014-07-09 20:24:38 +1000

    Finish Off Ram Overclock Again

--- arch/arm/mach-tegra/board-stingray-memory.c
+++ arch/arm/mach-tegra/board-stingray-memory.c
@@ -744,7 +744,7 @@ static const struct tegra_emc_table stingray_emc_tables_elpida_40nm[] = {
 		}
 	},
 	{
-		.rate = 300000,   /* SDRAM frequency */
+		.rate = 400000,   /* SDRAM frequency */
 		.regs = {
 			0x00000012,   /* RC */
 			0x00000027,   /* RFC */
